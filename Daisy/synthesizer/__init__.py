e7c241a47c0a6428203b1a96f5af128310fb0911
Echoid <youranzhou0724@gmail.com>
2022-01-10 11:14:45 +1100

    initial

---
+++ Daisy/synthesizer/__init__.py
@@ -0,0 +1,4 @@
+from .model import VGAN_generator, VGAN_discriminator
+from .model import LGAN_generator, LGAN_discriminator, LSTM_discriminator
+from .model import DCGAN_generator, DCGAN_discriminator
+from .train import V_Train, W_Train, C_Train_dp, C_Train_nofair
